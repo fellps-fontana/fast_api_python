e0a8d1e2f76d19993aa04093edd57bad74777eaa
Felipe Locatelli Fontana <felipe_fontana@estudante.sesisenai.sc.org.br>
2025-02-06 10:28:13 -0300

    Commit inicial

--- fest_api_zero/app.py
+++ fest_api_zero/app.py
@@ -1,8 +1,33 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+from fest_api_zero.schemas import Message
+
 
 app = FastAPI()
 
-
-@app.get('/')
+@app.get("/", response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
-    return {'message': 'Olá mundo veio'}
+    return {"message": "Olá mundo veio"}
+
+
+@app.get("/EXE", response_class=HTMLResponse, status_code=HTTPStatus.OK)
+def read_html():
+    return """
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content7u88888888888888888="width=device-width, initial-scale=1.0">
+        <title>Exercicio Requisicao </title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Olá, Mundo Veio</h1>
+            <a href="#" class="btn btn-custom">
+                <i class="fas fa-play"></i> Explodir mundo
+            </a>
+        </div>
+    </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,9 +7,27 @@ from fest_api_zero.app import app
 
 def test_read_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)
-
     response = client.get("/")
-
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "Olá mundo veio"}
 
-    assert response.json() == {"message": 'Olá mundo veio'}
+
+def test_read_html_deve_retornar_ok_e_html():
+    client = TestClient(app)
+    response = client.get("/EXE")
+    assert response.status_code == HTTPStatus.OK
+    assert response.text.strip() == (
+        '\n    <!DOCTYPE html>\n    <html lang="pt-br">\n    <head>\n'
+        '        <meta charset="UTF-8">\n'
+        '        <meta name="viewport" ' +
+        'content="width=device-width, initial-scale=1.0">' +
+        '\n'
+        '        <title>Exercicio Requisicao </title>\n'
+        '    </head>\n    <body>\n'
+        '        <div class="container">\n'
+        '            <h1>Olá, Mundo Veio</h1>\n'
+        '            <a href="#" class="btn btn-custom">\n'
+        '                <i class="fas fa-play"></i> Explodir mundo\n'
+        '            </a>\n'
+        '        </div>\n    </body>\n    </html>\n    '
+    ).strip()
